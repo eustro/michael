68f9517ae5dbe5e012baefa2914a20adc723de9d
eustro <eugen0stroh@googlemail.com>
2017-07-10 16:36:13 +0200

    Some changes to chronicle processing;

--- app/chronicle_processor.py
+++ app/chronicle_processor.py
@@ -30,52 +30,59 @@ class ChronicleProcessor:
         """
         page_width = original_size[1]
         image_type = self.conf.image_type
-        images = walk_dir(in_dir, file_type=image_type)
+        layout_components = walk_dir(in_dir, file_type=image_type)
         # Maybe no output by image processing, so need to be checked manually!
-        if not images or images == []:
+        if not layout_components or layout_components == []:
             raise Exception("No images found in {0}".format(in_dir))
 
         images = []
 
         text_fragments = {'political': [],
-                          'secular': [],
+                          'misc': [],
                           'eccles': [],
                           'footnotes': []}
 
-        for box in images:
-            im = imread(box, as_grey=True)
+        for component in layout_components:
+            im = imread(component, as_grey=True)
             if im is None:
-                print('{0} not found'.format(box))
+                print('{0} not found'.format(component))
                 continue
-            images.append({'box': box, 'image': im})
+            images.append({'component': component, 'image': im})
+        if len(images) == 2:
+            text_fragments['footnotes'].append(images[1]['component'])
+        if images[0]['image'].shape[1] / page_width >= 0.5:
+            text_fragments['political'].append(images[0]['component'])
         # Two columns: Secular, ecclesial. Footnotes beneath.
-        if len(images) == 3:
+        elif len(images) == 3:
             # Label footnotes
-            text_fragments['footnotes'].append(images[2]['box'])
+            text_fragments['footnotes'].append(images[2]['component'])
             if images[1]['image'].shape[1] / page_width < 0.5:
-                text_fragments['secular'].append(images[1]['box'])
+                text_fragments['misc'].append(images[1]['component'])
+            else:
+                text_fragments['political'].append(images[1]['component'])
             if images[0]['image'].shape[1] / page_width < 0.5:
-                text_fragments['eccles'].append(images[0]['box'])
+                text_fragments['eccles'].append(images[0]['component'])
+            else:
+                text_fragments['political'].append(images[0]['component'])
         # Political, secular, ecclesial
-        if len(images) == 4:
-            text_fragments['footnotes'].append(images[2]['box'])
+        elif len(images) == 4:
             # Label footnotes
-            text_fragments['footnotes'].append(images[3]['box'])
+            text_fragments['footnotes'].append(images[3]['component'])
 
             # Arrange ecclesial part
             if images[2]['image'].shape[1] / page_width >= 0.5:
-                text_fragments['political'].append(images[2]['box'])
+                text_fragments['political'].append(images[2]['component'])
             else:
-                text_fragments['secular'].append(images[2]['box'])
+                text_fragments['eccles'].append(images[2]['component'])
 
             # Arrange secular part
             if images[1]['image'].shape[1] / page_width >= 0.5:
-                text_fragments['political'].append(images[1]['box'])
+                text_fragments['political'].append(images[1]['component'])
             else:
-                text_fragments['eccles'].append(images[1]['box'])
+                text_fragments['misc'].append(images[1]['component'])
 
             # Arrange political part
-            text_fragments['political'].append(images[0]['box'])
+            text_fragments['political'].append(images[0]['component'])
         # We can not be sure, with what column of the chronicle we are dealing with.
         # Case 1 part: There were probably more than one layout component merged together.
         # Case 2 parts: Probably one big column and footnotes, but we can't be sure which parts there are.
@@ -87,14 +94,14 @@ class ChronicleProcessor:
             if in_dir.endswith('/'):
                 new_name = in_dir[:-1]
             new_name += '_unchecked/'
-            if '_unchecked/' not in in_dir:
+            if '_unchecked' not in in_dir:
                 rename(in_dir, new_name)
 
         return text_fragments
 
-    def _rename_fragments(self, fragments: dict) -> bool:
+    def _rename_fragments(self, fragments: dict):
         if not fragments:
-            return False
+            raise Exception('Fragments empty: {0}'.format(fragments))
 
         for frag, file_list in fragments.items():
             if not file_list:
@@ -104,23 +111,23 @@ class ChronicleProcessor:
                 file_name, file_ext = file_parts
                 new_name = dirname(file) + '/' + file_name + '_' + str(frag) + '.' + file_ext
                 if frag not in file:
-                    rename(file, new_name)
-
-        return True
+                    try:
+                        rename(file, new_name)
+                    except:
+                        raise Exception("Could not rename layout component {0} to new name: {1}".format(file, new_name))
 
     def __process_page(self, in_dir) -> bool:
         file_type = self.conf.image_type
         dirs = list_sub_dirs(in_dir)
         if not dirs:
-            return False
-
+            raise Exception('Directory structure does not seem to be correct.')
         for sub_dir in dirs:
-            page = walk_dir(dirname(sub_dir), file_type=file_type)
-            if page:
-                page = page[0]
+            pages = walk_dir(dirname(sub_dir), file_type=file_type)
+            if pages:
+                pages = pages[0]
             else:
-                return False
-            original_size = imread(page).shape
+                raise Exception('Could not find corresponding original page for {0}'.format(sub_dir))
+            original_size = imread(pages).shape
             fragments = self.__arrange_text(sub_dir, original_size)
             self._rename_fragments(fragments)
         return True
@@ -128,11 +135,20 @@ class ChronicleProcessor:
     def __process_chronicle(self) -> bool:
         in_dir = self.conf.in_dir
         dirs = list_sub_dirs(in_dir)
+        if self.conf.verbose:
+            print("++ Chronicle Processor ++")
+            print("   Processing file(s) in path " + in_dir)
         if not dirs:
-            return False
+            raise Exception('Directory structure does not seem to be correct: {0}'.format(in_dir))
 
         for sub_dir in dirs:
+            if self.conf.verbose:
+                print("    ... " + sub_dir + " ... ")
             self.__process_page(sub_dir)
+
+        if self.conf.verbose:
+            print("++++++++++++++++++++++")
+
         return True
 
     def run(self) -> None:
